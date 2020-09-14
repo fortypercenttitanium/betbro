230ba782f3ea41860c82a62be3a855bd419f9ed3
Alex Younger <alexanderyounger@gmail.com>
2020-09-14 10:59:55 -0400

    added logger

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 8000;
 const scrapers = require('./src/tools/scrapers');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 scrapers.getStats();
 scrapers.fetchOdds();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+	path.join(__dirname, 'logger.log'),
+	{ flags: 'a' }
+);
+
+// setup the logger
+const logger = morgan('combined', { stream: accessLogStream });
+
+app.use(logger, express.static(path.join(__dirname, 'build')));
 
 app.get('/fetchStats', (req, res) => {
 	res.sendFile(path.join(__dirname, '/api/statsMaster.json'));

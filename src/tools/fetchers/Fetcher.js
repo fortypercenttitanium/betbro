48004af25ced38161936b414c2e8d8c53f6c16de
Alex Younger <alexanderyounger@gmail.com>
2021-11-29 13:48:31 -0500

    add fetcher classes

---
+++ src/tools/fetchers/Fetcher.js
@@ -0,0 +1,26 @@
+export default class JsonFetcher {
+  baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_DEV_URL
+      : process.env.REACT_APP_URL;
+
+  async fetch(url) {
+    try {
+      const query = await fetch(url);
+
+      if (query.ok) {
+        const result = await query.json();
+
+        if (result) {
+          return result;
+        }
+      }
+
+      return {
+        error: 'Failed to fetch',
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}

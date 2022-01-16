7840e899459f5e6a1ed96abf9b81bc4a0cecb06b
Alex Younger <alexanderyounger@gmail.com>
2022-01-16 13:46:28 -0500

    change routes for api

--- src/tools/fetchers/StatsFetcher.js
+++ src/tools/fetchers/StatsFetcher.js
@@ -3,7 +3,7 @@ import Fetcher from './Fetcher';
 export default class StatsFetcher extends Fetcher {
   async getStats() {
     try {
-      return await this.fetch(this.baseUrl + '/stats');
+      return await this.fetch(this.baseUrl + '/v1/nfl/teamStats');
     } catch (err) {
       console.error(err);
     }

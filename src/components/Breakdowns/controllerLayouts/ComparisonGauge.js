65533cfff1144a7bb3a9d56710b67e2ee240bc4b
Alex Younger <alexanderyounger@gmail.com>
2022-02-04 09:58:08 -0500

    fix potential bug in comparison bug

--- src/components/Breakdowns/controllerLayouts/ComparisonGauge.js
+++ src/components/Breakdowns/controllerLayouts/ComparisonGauge.js
@@ -1,5 +1,41 @@
 import React from 'react';
 
+const teamColors = {
+  'Arizona Cardinals': '#97233F',
+  'Atlanta Falcons': '#A71930',
+  'Baltimore Ravens': '#241773',
+  'Buffalo Bills': '#00338D',
+  'Carolina Panthers': '#0085CA',
+  'Chicago Bears': '#0B162A',
+  'Cincinnati Bengals': '#FB4F14',
+  'Cleveland Browns': '#311D00',
+  'Dallas Cowboys': '#003594',
+  'Denver Broncos': '#FB4F14',
+  'Detroit Lions': '#0076B6',
+  'Green Bay Packers': '#203731',
+  'Houston Texans': '#03202F',
+  'Indianapolis Colts': '#002C5F',
+  'Jacksonville Jaguars': '#101820',
+  'Kansas City Chiefs': '#E31837',
+  'Los Angeles Chargers': '#002A5E',
+  'Los Angeles Rams': '#003594',
+  'Las Vegas Raiders': '#000000',
+  'Miami Dolphins': '#008E97',
+  'Minnesota Vikings': '#4F2683',
+  'New England Patriots': '#002244',
+  'New Orleans Saints': '#D3BC8D',
+  'New York Giants': '#0B2265',
+  'New York Jets': '#125740',
+  'Philadelphia Eagles': '#004C54',
+  'Pittsburgh Steelers': '#FFB612',
+  'Seattle Seahawks': '#69BE28',
+  'San Francisco 49ers': '#AA0000',
+  'Tampa Bay Buccaneers': '#D50A0A',
+  'Tennessee Titans': '#0C2340',
+  'Washington Football Team': '#773141',
+  None: '#a3a3a3',
+};
+
 export default function ComparisonGauge({
   homeRank,
   awayRank,
@@ -10,45 +46,10 @@ export default function ComparisonGauge({
     awayRank && homeRank
       ? Math.round((awayRank - homeRank + 41) * 1.52) - 2
       : 60;
-  const teamColors = {
-    'Arizona Cardinals': '#97233F',
-    'Atlanta Falcons': '#A71930',
-    'Baltimore Ravens': '#241773',
-    'Buffalo Bills': '#00338D',
-    'Carolina Panthers': '#0085CA',
-    'Chicago Bears': '#0B162A',
-    'Cincinnati Bengals': '#FB4F14',
-    'Cleveland Browns': '#311D00',
-    'Dallas Cowboys': '#003594',
-    'Denver Broncos': '#FB4F14',
-    'Detroit Lions': '#0076B6',
-    'Green Bay Packers': '#203731',
-    'Houston Texans': '#03202F',
-    'Indianapolis Colts': '#002C5F',
-    'Jacksonville Jaguars': '#101820',
-    'Kansas City Chiefs': '#E31837',
-    'Los Angeles Chargers': '#002A5E',
-    'Los Angeles Rams': '#003594',
-    'Las Vegas Raiders': '#000000',
-    'Miami Dolphins': '#008E97',
-    'Minnesota Vikings': '#4F2683',
-    'New England Patriots': '#002244',
-    'New Orleans Saints': '#D3BC8D',
-    'New York Giants': '#0B2265',
-    'New York Jets': '#125740',
-    'Philadelphia Eagles': '#004C54',
-    'Pittsburgh Steelers': '#FFB612',
-    'Seattle Seahawks': '#69BE28',
-    'San Francisco 49ers': '#AA0000',
-    'Tampa Bay Buccaneers': '#D50A0A',
-    'Tennessee Titans': '#0C2340',
-    'Washington Football Team': '#773141',
-    None: '#a3a3a3',
-  };
   const circleColor =
     teamColors[
       awayRank < homeRank ? awayTeam : awayRank > homeRank ? homeTeam : 'None'
-    ];
+    ] || '#a3a3a3';
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"

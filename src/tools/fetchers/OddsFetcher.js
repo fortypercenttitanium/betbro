55ecbc4e44b2e632bb5f46ff18092d68f3f7c95f
Alex Younger <alexanderyounger@gmail.com>
2022-10-17 18:39:29 -0400

    add test data on client

--- src/tools/fetchers/OddsFetcher.js
+++ src/tools/fetchers/OddsFetcher.js
@@ -1,8 +1,11 @@
 import Fetcher from './Fetcher';
+import testOdds from '../../testData/testOdds.json';
 
 export default class OddsFetcher extends Fetcher {
   async getOdds() {
     try {
+      if (process.env.REACT_APP_TESTING === 'true') return testOdds;
+
       return await this.fetch(
         this.baseUrl + `/odds?testing=${this.testing.toString()}`,
       );
--- src/tools/fetchers/StatsFetcher.js
+++ src/tools/fetchers/StatsFetcher.js
@@ -1,8 +1,11 @@
 import Fetcher from './Fetcher';
+import testStats from '../../testData/testStats.json';
 
 export default class StatsFetcher extends Fetcher {
   async getStats() {
     try {
+      if (process.env.REACT_APP_TESTING === 'true') return testStats;
+
       return await this.fetch(
         this.baseUrl + `/v1/nfl/teamStats?testing=${this.testing.toString()}`,
       );
